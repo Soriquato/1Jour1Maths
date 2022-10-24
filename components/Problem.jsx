9eb592526e9c27b719756187526d73c674041a99
soriquato <maxencevalere@gmail.com>
2022-10-24 10:57:26 +0200

    Feat: Problem Component

--- components/Problem.jsx
+++ components/Problem.jsx
@@ -1,10 +1,22 @@
-import { MathJax, MathJaxContext } from "better-react-mathjax"
+import { useState } from "react";
+import Script from "next/script";
 
 export default function Problem(){
-    //let problem = 
+    const [loaded, setLoaded] = useState(false);
+    let textProblem = "When `a != 0`, there are two solutions to `ax^2 + bx + c = 0` and they are `x = (-b +- sqrt(b^2-4ac))/(2a) .`"
     return (
-        <MathJaxContext>
-            <p></p>
-        </MathJaxContext>
+        <div>
+            <Script type="text/javascript" src="../utils/test.js"></Script>
+            <Script
+                onLoad={() => setLoaded(true)}
+                async
+                type="text/javascript"
+                id="MathJax-script"
+                src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js"
+            ></Script>
+            <p style={{ display: loaded ? "initial" : "none" }}>
+                {textProblem}
+            </p>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react"
+import Problem from "../components/Problem";
 
 export default function Home() {
-  const [text, setText] = useState("")
-  useEffect(() => setText("When `a != 0`, there are two solutions to `ax^2 + bx + c = 0` and they are `x = (-b +- sqrt(b^2-4ac))/(2a) .`", []))
   return (
-      <div>
-        <script type="text/javascript" src='../utils/test.js'></script>
-        <script type="text/javascript" src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-        <script async type="text/javascript" id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js">
-        </script>
-        <p>{text}</p>
-      </div>
-  )
+    <Problem></Problem>
+  );
 }
