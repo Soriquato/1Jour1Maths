a21cc3be02905638dd85ae800e6cec059e0954dd
soriquato <maxencevalere@gmail.com>
2022-10-24 11:12:21 +0200

    Feat: BetterNextMathJax module

--- components/Problem.jsx
+++ components/Problem.jsx
@@ -1,22 +1,18 @@
 import { useState } from "react";
 import Script from "next/script";
+import { MathJaxContext, MathJax } from "better-react-mathjax";
 
 export default function Problem(){
-    const [loaded, setLoaded] = useState(false);
-    let textProblem = "When `a != 0`, there are two solutions to `ax^2 + bx + c = 0` and they are `x = (-b +- sqrt(b^2-4ac))/(2a) .`"
+    const config = {
+        loader: { load: ["input/asciimath"] }
+    };
+    let textProblem = ""
     return (
         <div>
-            <Script type="text/javascript" src="../utils/test.js"></Script>
-            <Script
-                onLoad={() => setLoaded(true)}
-                async
-                type="text/javascript"
-                id="MathJax-script"
-                src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js"
-            ></Script>
-            <p style={{ display: loaded ? "initial" : "none" }}>
-                {textProblem}
-            </p>
+            <MathJaxContext config={config}>
+            <h2>Basic MathJax example with AsciiMath</h2>
+            <MathJax>{"When `a != 0`, there are two solutions to `ax^2 + bx + c = 0` and they are `x = (-b +- sqrt(b^2-4ac))/(2a) .`"}</MathJax>
+            </MathJaxContext>
         </div>
     )
 }
