66f27563c0a82bdc3fd98d3b0a6b7ed92be0493c
Soriquato <maxence.valere@gmail.com>
2022-11-15 21:14:57 +0100

    MathTextInput change working

--- components/Problem.jsx
+++ components/Problem.jsx
@@ -1,9 +1,10 @@
 import MathText from "./MathText";
+import style from "../styles/Problem.module.css"
 
 export default function Problem(){
     let textProblem = "`sum_(i=1)^n i^3=((n(n+1))/2)^2`"
     return (
-        <div>
+        <div className={style.problemWrapper}>
             <MathText>{textProblem}</MathText>
         </div>
     )
